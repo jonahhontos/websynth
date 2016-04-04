93cb69db0b767e303ce6f3f9bd2cc7f543a27313
Jonah Hontos <jonahhontos@gmail.com>
2016-04-04 13:56:26 -0700

    basic patch model

---
+++ models/Patch.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema
+
+
+// ---- Schemas ---- //
+// - vcos - //
+var vcoSchema = new Schema({
+  type: String,
+  detune: Number,
+  gain: Number
+})
+
+// - envelope - //
+var adsrSchema = new Schema({
+  attack: Number,
+  decay: Number,
+  sustain: Number,
+  release: Number
+})
+
+// - filter - //
+var filterSchema = new Schema({
+  type: String,
+  resonance: Number,
+  cutoff: Number
+})
+
+// - lfos - //
+var lfoSchema = new Schema({
+  parameter: String,
+  amount: Number,
+  frequency: Number
+})
+
+// - effects go here - //
+
+// - patch - //
+var patchSchema = new Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  vcos: [vcoSchema],
+  ampAdsr: adsrSchema,
+  filter: filterSchema,
+  filterAdsr: adsrSchema,
+  lfos: [lfoSchema]
+})
+
+
+// ---- Export Module ---- //
+var Patch = mongoose.model('Patch', patchSchema)
+
+module.exports = Patch
